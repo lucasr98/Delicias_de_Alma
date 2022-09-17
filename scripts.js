d500b57d5c8bc236784ec431598bf717a8f45c3e
lucasr98 <lucascalavera@hotmail.com>
2022-09-17 19:30:04 -0300

    17/09/22 (Nuevas imágenes en la animación del background)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const backgroundImgUno = document.getElementById("backgroundImgUno"),
 backgroundImgDos = document.getElementById("backgroundImgDos"),
-backgroundImgTres = document.getElementById("backgroundImgTres");
+backgroundImgTres = document.getElementById("backgroundImgTres"),
+backgroundImgCuatro = document.getElementById("backgroundImgCuatro"),
+backgroundImgCinco = document.getElementById("backgroundImgCinco");
 
 let contador = 0;
 
@@ -9,22 +11,41 @@ function cambiarBackground(){
 		backgroundImgUno.style.opacity = "1";
 		backgroundImgDos.style.opacity = "0";
 		backgroundImgTres.style.opacity = "0";
+		backgroundImgCuatro.style.opacity = "0";
+		backgroundImgCinco.style.opacity = "0";
 		contador = 1;
-		console.log(contador)
 	}
 	else if(contador == 1){
 		backgroundImgUno.style.opacity = "0";
 		backgroundImgDos.style.opacity = "1";
 		backgroundImgTres.style.opacity = "0";
+		backgroundImgCuatro.style.opacity = "0";
+		backgroundImgCinco.style.opacity = "0";
 		contador = 2;
-		console.log(contador)
 	}
 	else if(contador == 2){
 		backgroundImgUno.style.opacity = "0";
 		backgroundImgDos.style.opacity = "0";
 		backgroundImgTres.style.opacity = "1";
+		backgroundImgCuatro.style.opacity = "0";
+		backgroundImgCinco.style.opacity = "0";
+		contador = 3;
+	}
+	else if(contador == 3){
+		backgroundImgUno.style.opacity = "0";
+		backgroundImgDos.style.opacity = "0";
+		backgroundImgTres.style.opacity = "0";
+		backgroundImgCuatro.style.opacity = "1";
+		backgroundImgCinco.style.opacity = "0";
+		contador = 4;
+	}
+	else if(contador == 4){
+		backgroundImgUno.style.opacity = "0";
+		backgroundImgDos.style.opacity = "0";
+		backgroundImgTres.style.opacity = "0";
+		backgroundImgCuatro.style.opacity = "0";
+		backgroundImgCinco.style.opacity = "1";
 		contador = 0;
-		console.log(contador)
 	}
 }
 
